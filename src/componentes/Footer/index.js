def270153d6814baeae97dc6ee4d7398cb15ad06
Samir Cavalcanti de Oliveira <107083863+DevSamirColiveira@users.noreply.github.com>
2023-09-16 11:56:36 -0300

    atlz

--- src/componentes/Footer/index.js
+++ src/componentes/Footer/index.js
@@ -4,14 +4,14 @@ import { FooterBase } from './styles';
 export default function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
+      {/* <a href="https://www.alura.com.br/">
         <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
-      </a>
+      </a> */}
       <p>
         Orgulhosamente criado durante a
         {' '}
-        <a href="https://www.alura.com.br/">
-          Imersão React da Alura
+        <a href="https://www.linkedin.com/in/samir-oliveira-dev/">
+          Imersão React e Challenge do Aluraflix
         </a>
       </p>
     </FooterBase>
